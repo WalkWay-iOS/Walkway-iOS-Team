a9ae90ae80ffa9cd31051e0f957ddbbffba55009
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-02-01 20:37:01 +0900

    [Feat] AlertView 세팅완료

---
+++ Walkway_iOS/Walkway_iOS/Source/AlertScene/View/Cell/TVC/AlertTVC.swift
@@ -0,0 +1,54 @@
+//
+//  AlertTVC.swift
+//  Walkway_iOS
+//
+//  Created by SHIN YOON AH on 2021/02/01.
+//
+
+import UIKit
+
+class AlertTVC: UITableViewCell {
+    static let identifier = "AlertTVC"
+
+    @IBOutlet weak var profileImage: UIButton!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUI()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
+
+// MARK: - UI
+extension AlertTVC {
+    private func setUI() {
+        setBackground()
+        setButton()
+        setLabel()
+    }
+    
+    private func setBackground() {
+        backgroundColor = .clear
+    }
+    
+    private func setButton() {
+        profileImage.layer.cornerRadius = 21
+        profileImage.isUserInteractionEnabled = false
+    }
+    
+    private func setLabel() {
+        contentLabel.font = .systemFont(ofSize: 13, weight: .medium)
+        contentLabel.textColor = .gray70
+    }
+}
+
+// MARK: - Data
+extension AlertTVC {
+    func setData(text: String) {
+        contentLabel.text = text
+    }
+}
--- Walkway_iOS/Walkway_iOS/Source/AlertScene/View/VC/AlertVC.swift
+++ Walkway_iOS/Walkway_iOS/Source/AlertScene/View/VC/AlertVC.swift
@@ -8,22 +8,98 @@
 import UIKit
 
 class AlertVC: UIViewController {
-
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var alertButton: UIButton!
+    @IBOutlet weak var messageButton: UIButton!
+    @IBOutlet weak var alertTableView: UITableView!
+    
+    var alerts: [String] = ["따릉이 님이 [ 광화문을 지나는 북악산 트래킹 ] 를 좋아합니다 !", "알림창만들기 님이 [ 서울숲에서 놀자 ] 를 북마크 했습니다 !", "알림창만들기 님이 당신을 팔로우 했습니다.", "따릉이 님이 [ 광화문을 지나는 북악산 트래킹 ] 를 좋아합니다 !", "알림창만들기 님이 [ 서울숲에서 놀자 ] 를 북마크 했습니다 !", "알림창만들기 님이 당신을 팔로우 했습니다.", "방금 새로운 뱃지를 획득했습니다! 확인해보세요!", "WalkWay를 완료했습니다! 기록을 확인해보세요!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - TableViewDelegate
+extension AlertVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alerts.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AlertTVC.identifier) as? AlertTVC else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+        cell.setData(text: alerts[indexPath.row])
+        return cell
     }
-    */
+}
 
+extension AlertVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+}
+
+// MARK: - UI
+extension AlertVC {
+    private func setUI() {
+        setTableView()
+        setTableViewNib()
+        setButton()
+    }
+    
+    private func setTableView() {
+        alertTableView.delegate = self
+        alertTableView.dataSource = self
+        alertTableView.backgroundColor = .gray30
+        alertTableView.separatorColor = .clear
+    }
+    
+    private func setTableViewNib() {
+        let alertNib = UINib(nibName: "AlertTVC", bundle: nil)
+        alertTableView.register(alertNib, forCellReuseIdentifier: AlertTVC.identifier)
+    }
+    
+    private func setButton() {
+        profileButton.layer.cornerRadius = 21
+        profileButton.layer.shadowColor = UIColor.black.withAlphaComponent(0.8).cgColor
+        profileButton.layer.shadowOpacity = 1.0
+        profileButton.layer.shadowOffset = CGSize(width: 1, height: 2)
+        profileButton.layer.shadowRadius = 3
+        
+        alertButton.addTarget(self, action: #selector(touchUpAlert), for: .touchUpInside)
+        messageButton.addTarget(self, action: #selector(touchUpMessage), for: .touchUpInside)
+        
+        selectedAlertButton()
+    }
+    
+    private func selectedAlertButton() {
+        alertButton.backgroundColor = .gray30
+        alertButton.setTitle("", for: .normal)
+        
+        messageButton.backgroundColor = .white
+        messageButton.setTitle("", for: .normal)
+    }
+    
+    private func selectedMessageButton() {
+        alertButton.backgroundColor = .white
+        alertButton.setTitle("", for: .normal)
+        
+        messageButton.backgroundColor = .gray30
+        messageButton.setTitle("", for: .normal)
+    }
+}
+
+// MARK: - Action
+extension AlertVC {
+    @objc func touchUpAlert() {
+        selectedAlertButton()
+    }
+    
+    @objc func touchUpMessage() {
+        selectedMessageButton()
+    }
 }
