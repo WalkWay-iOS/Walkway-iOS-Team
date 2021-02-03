ded7864d07ea2aa9619203075816dc2d0ab06622
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-02-03 15:05:08 +0900

    [Feat] 팔로워View HeaderCell 세팅완료

---
+++ Walkway_iOS/Walkway_iOS/Source/HomeScene/View/Cell/TVC/Follower/FollowerHeaderTVC.swift
@@ -0,0 +1,113 @@
+//
+//  FollowerHeaderTVC.swift
+//  Walkway_iOS
+//
+//  Created by SHIN YOON AH on 2021/02/03.
+//
+
+import UIKit
+
+class FollowerHeaderTVC: UITableViewCell {
+    static let identifier = "FollowerHeaderTVC"
+    
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var followingTitleLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var courseTitleLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var followerTitleLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followButton: UIButton!
+    
+    var isFollowing =  false
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUI()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
+
+// MARK: - UI
+extension FollowerHeaderTVC {
+    private func setUI() {
+        setView()
+        setImageView()
+        setLabel()
+        setButton()
+    }
+    
+    private func setView() {
+        self.backgroundColor = .bookmarkDarkBlue
+        bottomView.backgroundColor = .gray40
+    }
+    
+    private func setImageView() {
+        profileImage.layer.cornerRadius = 45
+        profileImage.layer.masksToBounds = false
+        profileImage.layer.shadowColor = UIColor.black.withAlphaComponent(0.8).cgColor
+        profileImage.layer.shadowOpacity = 1.0
+        profileImage.layer.shadowOffset = CGSize(width: 2, height: 3)
+        profileImage.layer.shadowRadius = 2
+    }
+    
+    private func setLabel() {
+        nameLabel.font = .boldSystemFont(ofSize: 15)
+        
+        followingTitleLabel.text = "팔로잉"
+        followingTitleLabel.font = .systemFont(ofSize: 13, weight: .semibold)
+        
+        followingLabel.text = "17"
+        followingLabel.font = .boldSystemFont(ofSize: 20)
+        
+        courseTitleLabel.text = "코스"
+        courseTitleLabel.font = .systemFont(ofSize: 13, weight: .semibold)
+        
+        courseLabel.text = "9"
+        courseLabel.font = .boldSystemFont(ofSize: 20)
+        
+        followerTitleLabel.text = "팔로워"
+        followerTitleLabel.font = .systemFont(ofSize: 13, weight: .semibold)
+        
+        followerLabel.text = "132"
+        followerLabel.font = .boldSystemFont(ofSize: 20)
+    }
+    
+    private func setButton() {
+        followButton.titleLabel?.font = .systemFont(ofSize: 11, weight: .semibold)
+        followButton.layer.cornerRadius = 10
+        followButton.contentEdgeInsets = UIEdgeInsets(top: 3, left: 12, bottom: 3, right: 12)
+        followButton.addTarget(self, action: #selector(touchUpFollow), for: .touchUpInside)
+        followSetting()
+    }
+    
+    private func followSetting() {
+        if isFollowing {
+            followButton.setTitle("언팔로우", for: .normal)
+            followButton.backgroundColor = .gray50
+            followButton.setTitleColor(.bookmarkBlue, for: .normal)
+        } else {
+            followButton.setTitle("팔로우", for: .normal)
+            followButton.backgroundColor = .latestBlue
+            followButton.setTitleColor(.gray70, for: .normal)
+        }
+    }
+}
+
+// MARK: - Action
+extension FollowerHeaderTVC {
+    @objc func touchUpFollow() {
+        if isFollowing {
+            isFollowing = false
+            followSetting()
+        } else {
+            isFollowing = true
+            followSetting()
+        }
+    }
+}
--- Walkway_iOS/Walkway_iOS/Source/HomeScene/View/VC/FollowerVC.swift
+++ Walkway_iOS/Walkway_iOS/Source/HomeScene/View/VC/FollowerVC.swift
@@ -10,27 +10,93 @@ import UIKit
 class FollowerVC: UIViewController {
     @IBOutlet weak var followerTableView: UITableView!
     
-    let closeButton: UIButton = {
-        let closeButton = UIButton()
-        closeButton.translatesAutoresizingMaskIntoConstraints = false
-        closeButton.setImage(UIImage(systemName: "xmark"), for: .normal)
-        closeButton.tintColor = .bookmarkDarkBlue
-        closeButton.setPreferredSymbolConfiguration(.init(pointSize: 18, weight: .regular, scale: .large), forImageIn: .normal)
-        return closeButton
-    }()
+    let closeButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+    }
+}
+
+// MARK: - TableViewDelegate
+extension FollowerVC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FollowerHeaderTVC.identifier) as? FollowerHeaderTVC else {
+            return UITableViewCell()
+        }
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
+extension FollowerVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 376
+        }
+        return 0
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.closeButton.isHidden = true
+        })
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.closeButton.isHidden = false
+        })
     }
 }
 
 // MARK: - UI
 extension FollowerVC {
     private func setUI() {
-        
+        setView()
+        setTableView()
+        setTableViewNib()
+        setButton()
+    }
+    
+    private func setView() {
+        view.backgroundColor = .bookmarkDarkBlue
+    }
+    
+    private func setTableView() {
+        followerTableView.delegate = self
+        followerTableView.dataSource = self
+        followerTableView.backgroundColor = .bookmarkDarkBlue
+    }
+    
+    private func setTableViewNib() {
+        let headerNib = UINib(nibName: "FollowerHeaderTVC", bundle: nil)
+        followerTableView.register(headerNib, forCellReuseIdentifier: FollowerHeaderTVC.identifier)
     }
     
     private func setButton() {
+        closeButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        closeButton.tintColor = .black
+        closeButton.setPreferredSymbolConfiguration(.init(pointSize: 18, weight: .regular, scale: .large), forImageIn: .normal)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.addTarget(self, action: #selector(touchUpClose), for: .touchUpInside)
+        view.addSubview(closeButton)
         closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
+        closeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    }
+}
+
+// MARK: - Action
+extension FollowerVC {
+    @objc func touchUpClose() {
+        dismiss(animated: true, completion: nil)
     }
 }
