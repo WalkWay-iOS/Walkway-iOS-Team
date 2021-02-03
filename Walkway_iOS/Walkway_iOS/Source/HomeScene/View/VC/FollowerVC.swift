37c8c250e886402d948d57f3e77906a9e2b874b3
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-02-03 13:14:23 +0900

    [Feat] 팔로우View 기본세팅

--- Walkway_iOS/Walkway_iOS/Source/HomeScene/View/VC/FollowerVC.swift
+++ Walkway_iOS/Walkway_iOS/Source/HomeScene/View/VC/FollowerVC.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class FollowerVC: UIViewController {
-
+    @IBOutlet weak var followerTableView: UITableView!
+    
+    let closeButton: UIButton = {
+        let closeButton = UIButton()
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        closeButton.tintColor = .bookmarkDarkBlue
+        closeButton.setPreferredSymbolConfiguration(.init(pointSize: 18, weight: .regular, scale: .large), forImageIn: .normal)
+        return closeButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: - UI
+extension FollowerVC {
+    private func setUI() {
+        
+    }
+    
+    private func setButton() {
+        closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
     }
 }
