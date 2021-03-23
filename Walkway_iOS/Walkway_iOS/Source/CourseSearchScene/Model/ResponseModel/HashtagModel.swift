0cadbc49758ef4931446f93f497fc07358a4e707
jjuyaa <smwujjuyaa@gmail.com>
2021-03-23 14:43:33 +0900

    [Feat] search서버 세팅완료

---
+++ Walkway_iOS/Walkway_iOS/Source/CourseSearchScene/Model/ResponseModel/HashtagModel.swift
@@ -0,0 +1,21 @@
+//
+//  HashtagModel.swift
+//  Walkway_iOS
+//
+//  Created by jjuyaa on 2021/03/23.
+//
+
+import Foundation
+
+// MARK: - HashtagModel
+struct HashtagModel: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: HashtagResponse
+}
+
+// MARK: - HashtagResponse
+struct HashtagResponse: Codable {
+    let hashtag: [Hashtag]
+}
